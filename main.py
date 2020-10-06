0ce16096249fbf0b7659733cd8e10303e6fa2cdc
Franklin Ikeh <69267493+fr4nkl1n-1k3h@users.noreply.github.com>
2020-10-06 20:07:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-'''
+"""
 Author: Aditya Mangal 
 Date:  17 september,2020
 Purpose: python mini project
 
-'''
-import string
-import random
+"""
+from random import choice, shuffle
+from string import (
+    ascii_letters as letters,
+    digits,
+    punctuation,
+)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # collect input
+    password_length = int(input("Enter the length of your password.\n"))
 
-    s = string.ascii_lowercase
-    # print(s)
-    s2 = string.ascii_uppercase
+    # generate a sequence list
+    character_sequence = [
+        choice(letters + digits + punctuation) for _ in range(password_length)
+    ]
 
-    s3 = string.digits
-    # print(s3)
+    # shuffle the sequence list
+    shuffle(character_sequence)
 
-    pass_len = int(input('Enter the length of your password.\n'))
-    pass_list = []
-    pass_list.extend(list(s))
-    pass_list.extend(list(s2))
-    pass_list.extend(list(s3))
+    # join the list into string
+    pass_generated = "".join(character_sequence)
 
-    random.shuffle(pass_list)
-    # print(pass_list)
-
-    pass_generated = "".join(pass_list[0:pass_len])
-
-    print(f'Password generated!\nYour password is {pass_generated}')
+    # output the string
+    print(f"Password generated!\nYour password is {pass_generated}")
